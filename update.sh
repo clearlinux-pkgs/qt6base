a5e935ef8a27598ffc460c327c2076974be3c32d
William Douglas <william.douglas@intel.com>
2025-04-03 01:42:58 -0700

    Update update.sh
    Rework update-version call for 2nd build of qt6base.
    
    Signed-off-by: William Douglas <william.douglas@intel.com>

--- update.sh
+++ update.sh
@@ -2,12 +2,20 @@
 
 set -eu -o pipefail
 
+base_round=0
 pkgs=('qt6base' 'qt6shadertools' 'qt6svg' 'qt6tools' 'qt6base' 'qt6declarative')
 for i in "${pkgs[@]}"; do
     pushd "../${i}"
     git fetch origin
     git reset --hard origin/main
-    make update-versions || if [ "$i" != 'qt6base' ]; then exit 1; fi
+    if [ "$i" = 'qt6base' ]; then
+        if [ $base_round -eq 0 ]; then
+            base_round=1
+            make update-versions
+        fi
+    else
+        make update-versions
+    fi
     make autospec
     make koji
     make koji-waitrepo
